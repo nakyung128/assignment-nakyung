174865fffb2473e3e998bc93bd761b21617c20a3
nakyung128 <lnk12888@naver.com>
2025-02-04 22:57:05 +0900

    ui: 검색 결과 List Item UI 구현

---
+++ app/src/main/java/com/nakyung/assignment_nakyung/components/RepoListItem.kt
@@ -0,0 +1,96 @@
+package com.nakyung.assignment_nakyung.components
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import coil.compose.AsyncImage
+import com.nakyung.assignment_nakyung.R
+import com.nakyung.assignment_nakyung.domain.model.Item
+
+@Composable
+fun RepoListItem(
+    modifier: Modifier = Modifier,
+    item: Item,
+) {
+    Column(
+        modifier =
+            modifier
+                .fillMaxWidth()
+                .padding(horizontal = 15.dp, vertical = 15.dp)
+                .background(color = Color.Unspecified),
+    ) {
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+        ) {
+            AsyncImage(
+                modifier =
+                    Modifier
+                        .size(size = 30.dp)
+                        .clip(shape = CircleShape),
+                model = item.owner.avatarUrl,
+                contentDescription = "owner_image",
+            )
+            Spacer(modifier = Modifier.width(8.dp))
+            Text(
+                text = item.owner.login,
+                color = Color.Gray,
+                fontSize = 18.sp,
+            )
+        }
+        Spacer(modifier = Modifier.height(5.dp))
+        Text(
+            text = item.name,
+            color = Color.Black,
+            fontSize = 20.sp,
+            fontWeight = FontWeight.Bold,
+        )
+        Spacer(modifier = Modifier.height(5.dp))
+        if (item.description != null) {
+            Text(
+                text = item.description,
+                color = Color.DarkGray,
+                fontSize = 20.sp,
+            )
+            Spacer(modifier = Modifier.height(10.dp))
+        }
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+        ) {
+            Icon(
+                painter = painterResource(id = R.drawable.ic_star),
+                contentDescription = "star",
+                tint = Color.Yellow,
+            )
+            Text(
+                modifier = Modifier.padding(start = 5.dp),
+                text = item.stargazersCount.toKFormat(),
+                color = Color.Gray,
+            )
+        }
+    }
+}
+
+fun Int.toKFormat(): String =
+    when {
+        this >= 1_000_000 -> String.format("%.1fM", this / 1_000_000.0)
+        this >= 1_000 -> String.format("%.1fK", this / 1_000.0)
+        else -> this.toString()
+    }
